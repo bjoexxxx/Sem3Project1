94d552b47ae651e49ec136d38bf242dfa70d8139
bjoexxxx <bjoe6702@stud.kea.dk>
2023-02-05 14:20:12 +0100

    How to datasource?

--- src/main/java/dat3/car/entity/Car.java
+++ src/main/java/dat3/car/entity/Car.java
@@ -1,7 +1,71 @@
 package dat3.car.entity;
 
-public class Car {
-    private String Brand;
-    private String Model;
+import jakarta.persistence.*;
 
+@Entity
+@Table (name = "car", schema = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "car_brand")
+    private String brand;
+    @Column(name = "car_model")
+    private String model;
+    @Column(name = "rental_price_day")
+    private double pricePerDay;
+    @Column(name = "max_discount")
+    private int bestDiscount;
+
+    public Car() {
+
+    }
+
+    public Car(Long id, String brand, String model, double pricePerDay, int bestDiscount){
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+        this.bestDiscount = bestDiscount;
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public int getBestDiscount() {
+        return bestDiscount;
+    }
+
+    public void setBestDiscount(int bestDiscount) {
+        this.bestDiscount = bestDiscount;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
